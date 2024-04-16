baf41b5f8075a1e29973780a34aefabdd41e6312
Voevodsky <167202952+Voevodsky@users.noreply.github.com>
2024-04-16 23:05:03 +0800

    Update reply_all.py

--- reply_all.py
+++ reply_all.py
@@ -2,33 +2,37 @@ import asyncio
 
 import aiotieba
 
+import time
+
+import logging
+logging.disable(logging.WARNING)
+
 BDUSS = "输入你的BDUSS"
 
 async def main():
+    start_time = time.time()  # 记录程序开始执行的时间
     async with aiotieba.Client(BDUSS) as client:
-        # 结果输出到文件output.txt中
+        uid = 输入待查询用户贴吧id
+        user =  await client.tieba_uid2user_info(uid)
+        print("查询用户：", user)
+        print("查询中...")
+        pn = 0
         with open("output.txt", "w", encoding="utf-8") as f:
-            tid = 待查询的帖子tid
-            pn = 1
-            while (1):
-                posts = await client.get_posts(tid, pn)
-                if pn == 1:
-                    f.write(f"贴吧名: {posts.thread.fname}\n\n")
-                    f.write(f"标题: {posts.thread.title}\n\n")
-                    f.write(f"发帖人: {posts.thread.user}\n\n")
-                    f.write("回复数： {}\n\n".format(posts.thread.reply_num))
-                    f.write("======================\n")
-                
-                for post in posts:
-                    f.write("楼层： {:<5}用户名: {:<20}  回复: {}\n\n".format(post.floor, post.user.user_name, post.text))
-                    comments = await client.get_comments(tid, post.pid)
-                    for comment in comments:
-                        f.write("楼中楼：             用户名: {:<20}   回复: {}\n\n".format(comment.user.user_name, comment.text))
-                    f.write("======================\n")
-                if posts.has_more:
-                    pn += 1
-                else:
+            while(1):
+                user_post = await client.get_user_posts(str(user), pn)
+                if user_post.objs == []:
+                    f.write("查询完毕, 共查询{}页\n".format(pn - 1))
+                    print("查询完毕, 共查询{}页\n".format(pn - 1))
                     break
-                        
-
+                
+                for x in user_post.objs:
+                    tieba_name = await client.get_fname(x.fid)
+                    for y in x:
+                        f.write("贴吧名: {} 主题帖id: {} 回复贴id: {}\n".format(tieba_name, y.tid, y.pid))
+                        f.write("   {:}\n".format(y.text))
+                pn += 1
+                if (pn % 20 == 0):
+                    print("查询页数", pn)
+    query_end_time = time.time() - start_time  # 记录查询结束与开始的时间差
+    print("总查询时间: {:.2f}秒".format(query_end_time))
 asyncio.run(main())
