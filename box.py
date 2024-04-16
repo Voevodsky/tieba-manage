7b864b264efaa0df7a08a61fae98a318acca086f
Voevodsky <167202952+Voevodsky@users.noreply.github.com>
2024-04-16 22:45:24 +0800

    Update box.py

--- box.py
+++ box.py
@@ -5,30 +5,29 @@ import aiotieba
 import logging
 logging.disable(logging.WARNING)
 
-BDUSS = "输入你的BDUSS"
+BDUSS = "你的BDUSS"
 
 async def main():
     async with aiotieba.Client(BDUSS) as client:
-        uid = 待查询的用户贴吧id
+        uid = 待用户贴吧id
         user =  await client.tieba_uid2user_info(uid)
         print("查询用户：", user)
         print("查询中...")
         pn = 0
-        # 输出至output.txtx
-        with open("output.txt", "w", encoding="utf-8") as f:
+        with open("./output/wuti1.txt", "w", encoding="utf-8") as f:
             while(1):
                 user_post = await client.get_user_posts(str(user), pn)
                 if user_post.objs == []:
-                    f.write("查询完毕\n")
+                    f.write("查询完毕, 共查询{}页\n".format(pn))
+                    print("查询完毕, 共查询{}页\n".format(pn))
                     break
                 
                 for x in user_post.objs:
+                    tieba_name = await client.get_fname(x.fid)
                     for y in x:
-                        tieba_name = await client.get_fname(y.fid)
-                        post_info = await client.get_posts(y.tid)
-                        f.write("贴吧名: {} 主题帖id: {} 主题帖: {}\n".format(tieba_name, y.tid, post_info.thread.title))
+                        f.write("贴吧名: {} 主题帖id: {} 回复贴id: {}\n".format(tieba_name, y.tid, y.pid))
                         f.write("   {:}\n".format(y.text))
                 pn += 1
-            # 终端输出ending表示查询完毕
-            print("ending")
+                if (pn % 20 == 0):
+                    print("查询页数", pn)
 asyncio.run(main())
